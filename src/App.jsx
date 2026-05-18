49adc7cf77ae87b55d1877e0178080657853bc5c
EITAN URIEL KOZE <49433681@ort>
2026-05-18 08:47:18 -0300

    Algunos import en app.jsx, components dentro de src y poco más

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { useState } from 'react'
+import Header from './components/Header.jsx'
+import Feed from './components/Feed.jsx'
+import Usuario from './components/Usuario.jsx'
+import PostDetallado from './components/PostDetallado.jsx'
+import './App.css'
+
