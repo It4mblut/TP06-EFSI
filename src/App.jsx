84f8e31030ba48524b430cb79e05ee7ac231ea5b
EITAN URIEL KOZE <49433681@ort>
2026-05-18 09:13:51 -0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,3 +5,29 @@ import Usuario from './components/Usuario.jsx'
 import PostDetallado from './components/PostDetallado.jsx'
 import './App.css'
 
+function App() {
+    const [vistaActual, setVistaActual] = useState('feed')
+    const [postSeleccionado, setPostSeleccionado] = useState(null)
+
+    function abrirPost(post) {
+        setPostSeleccionado(post)
+        setVistaActual('post')
+    }
+
+    function volverAlFeed() {
+        setPostSeleccionado(null)
+        setVistaActual('feed')
+    }
+
+    return (
+        <>
+            <Header vistaActual={vistaActual} setVistaActual={setVistaActual} />
+            {vistaActual === 'feed' && <Feed abrirPost={abrirPost} />}
+            {vistaActual === 'usuario' && <Usuario />}
+            {vistaActual === 'post' && postSeleccionado && <PostDetallado post={postSeleccionado} onVolver={volverAlFeed} />}
+
+        </>
+    )
+}
+
+export default App()
