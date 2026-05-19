685cda08937f51a64f6e9488b32495d325a3d2a7
EITAN URIEL KOZE <49433681@ort>
2026-05-19 14:53:59 -0300

    Retocando App.jsx por los nuevos cambios (header va a estar siempre asi que le saque)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header.jsx'
+import Sidebar from './components/Sidebar.jsx'
 import Feed from './components/Feed.jsx'
 import Usuario from './components/Usuario.jsx'
 import PostDetallado from './components/PostDetallado.jsx'
@@ -20,14 +21,18 @@ function App() {
     }
 
     return (
-        <>
-            <Header vistaActual={vistaActual} setVistaActual={setVistaActual} />
-            {vistaActual === 'feed' && <Feed abrirPost={abrirPost} />}
-            {vistaActual === 'usuario' && <Usuario />}
-            {vistaActual === 'post' && postSeleccionado && <PostDetallado post={postSeleccionado} onVolver={volverAlFeed} />}
-
-        </>
+      <>
+      <Header />
+      <div className="app-body">
+        <Sidebar setVistaActual={setVistaActual} />
+        <main className="app-main">
+          {vistaActual === 'feed' && <Feed abrirPost={abrirPost} />}
+          {vistaActual === 'usuario' && <Usuario />}
+          {vistaActual === 'post' && postSeleccionado && <PostDetallado post={postSeleccionado} onVolver={volverAlFeed} />}
+        </main>
+      </div>
+    </>
     )
 }
 
-export default App
+    export default App
